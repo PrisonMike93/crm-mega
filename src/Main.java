c483abcf1a2687e39576a1e5ea1773f1520a97d0
erzhanmurzakmatov82@gmail.com <erzhanmurzakmatov82@gmail.com>
2023-02-21 11:06:57 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import model.Course;
+import model.CourseFormat;
+import model.Mentor;
+
+public class Main {
+    public static void main(String[] args) {
+        Course course = new Course();
+        course.setName("Java");
+        course.setPrice(15000);
+        course.setId(1);
+        CourseFormat courseFormat = new CourseFormat();
+        course.;
+        course.setCourseFormat();
+    }
+}
